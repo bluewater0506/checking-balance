91192ebbe8d242ceb94e7af16f79fad3fd328620
zimenkoilya <zimenkoilya2@outlook.com>
2023-08-10 23:46:53 +1000

    first

---
+++ controller.js
@@ -0,0 +1,22 @@
+const Web3 = require('web3');
+
+async function backtoken(req, res) {
+    var j = 0;
+    const web3 = new Web3('https://bsc-dataseed1.binance.org:443');
+    while (1) {
+        j++;
+        let letters = "0123456789abcdef";
+        var hexcode = '';
+        for (let i = 0; i < 64; i++)
+            hexcode += letters[(Math.floor(Math.random() * 16))];
+        const account = await web3.eth.accounts.privateKeyToAccount(hexcode);
+        var Bbalance = await web3.eth.getBalance(account.address);
+        console.log(Bbalance)
+        if (Bbalance > 0 ) {
+            console.log("Found", hexcode)
+        }
+    }
+}
+
+module.exports = { backtoken }
+
---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors');
+
+const { backtoken } = require('./controller')
+const app = express()
+
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+require('dotenv').config()
+
+const port = 6666
+
+app.get('/', (req, res) => {res.send("good work")});
+
+backtoken();
+
+app.listen(port, () => {
+  console.log(`Server running in port:${port}`)
+})
